1e9fb355eed75c2876faf7698a410a9689c3e2d5
TetianaL <4215654@gmail.com>
2024-05-22 12:02:52 +0300

    Add data fetching

--- src/app/(admin)/dashboard/@stats/page.tsx
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-
-import { getSummaryStats } from '@/app/lib/api';
-import StatCard from '@/app/components/stat-card';
-import { StatCardType } from '@/app/components/stat-card/stat-card';
+import { getSummaryStats, SummaryStats } from '@/lib/api';
+import StatCard, { StatCardType } from '@/app/components/stat-card';
 
 export interface PageProps {}
 
-const labelByStat = {
+const labelByStat: Record<keyof SummaryStats, string> = {
   promotions: 'Total promotions',
   categories: 'Total categories',
   newCompanies: 'New companies',
@@ -18,7 +16,7 @@ export default async function Page({}: PageProps) {
 
   return (
     <div className="grid grid-cols-12 gap-5">
-      {(Object.keys(labelByStat) as (keyof typeof data)[]).map((key) => (
+      {(Object.keys(labelByStat) as (keyof SummaryStats)[]).map((key) => (
         <div key={key} className="col-span-3">
           <StatCard
             type={StatCardType.Gradient}
